bd1e2dbc2235b04c4d9c7ec78916b51748e5a886
KritikaE <kritika.one@gmail.com>
2025-09-18 00:23:00 +0530

    add and sub operations

--- operate.py
+++ operate.py
@@ -4,9 +4,5 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
-def mult(a, b):
-    return a * b
-
 print("add(5, 3):", add(5, 3))
 print("sub(5, 3):", sub(5, 3))
-print("mult(5, 3):", mult(5, 3))
