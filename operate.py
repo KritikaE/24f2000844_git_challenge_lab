ea1fd4719175cabdb1e06448ba15000967cf8c62
KritikaE <kritika.one@gmail.com>
2025-09-17 23:52:40 +0530

    in main

---
+++ operate.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mult(a, b):
+    return a * b
+
+print("add(5, 3):", add(5, 3))
+print("sub(5, 3):", sub(5, 3))
+print("mult(5, 3):", mult(5, 3))
